3e59c6d8dbb0e78db0c0a52222f7ca567d740673
Maksim Gonchar <mgonchar@sfedu.ru>
2023-11-10 16:45:43 +0300

    Добавьте файлы проекта.

---
+++ MaxGoncharLub5var4/MaxGoncharLub5var4.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	string text, in_text,out_text;
+	getline(cin, text); in_text = text;
+	int count=0;
+	while (in_text.find(" ") != -1) { count++; in_text.erase(in_text.find(" "), 1); }count++;
+	cout << count;
+
+}
